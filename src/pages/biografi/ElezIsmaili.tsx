ca6d48ab748148425a172437cdcd01c742d3bc18
redbil19 <redbil19@htl-shkoder.com>
2025-11-11 23:13:43 +0100

    Biografi

--- src/pages/biografi/ElezIsmaili.tsx
+++ src/pages/biografi/ElezIsmaili.tsx
@@ -4,8 +4,22 @@ const ElezIsmaili = () => {
   return (
     <BiografiTemplate
       name="Elez Ismaili"
-      title="Anëtar i Lëvizjes"
-      content="Biografia e Elez Ismailit do të shtohet së shpejti."
+      title="Shkencëtar dhe Publicist"
+      content={`Elez Ismaili lindi më 1.04.1952 në katundin Sëllarcë e Epërme, rrethina e Tetovës. Shkollën fillore e kreu në vendlindje, kurse të mesmen në Prishtinë, Medresenë e Mesme “Alauddin”, gjatë viteve 1967-1972. Studimet i kreu në Fakultetin Filozofik-Dega e Gjuhës dhe Letërsisë Shqipe (1972-1976). Studimet pasuniversitare i mbaroi më 1985; mori titullin shkencor Magjistër i Shkencave Filologjike. Pas diplomimit, më 1976, filloi me punë në vendlindje- profesor i gjuhës dhe letërsisë shqipe, deri më 1985, kur pranohet profesor në Medresenë e Mesme “Isa Beu”-Shkup, ku kaloi 31 vjet në mbëltimin e gjuhës amtare që nga gjenerata e parë deri më 2016, kur pensionohet.
+
+Përveç punës edukativo-arsimore, është marrë edhe me veprimtari shkencore e publicistike. Ka marrë pjesë nëpër simpoziume dhe sesione të ndryshme shkencore në Shkup, Tetovë, Prishtinë, Tiranë, Shkodër, Sarajevë, Teheran e Hamburg, Bern, Wil, Ofenbah. Është autor i skenarit të Akademisë Solemne “Gëzon Bota Islame” dhe i një sërë libretesh të manifestimeve fetare. Autor i skenarëve të dy dokumentarëve kulturor-arsimor. Është autor i një sërë shkrimesh shkencore e publicistike të botuara në faqet e “Hënës së Re”, “Diturisë Islame”, “Zanit të Naltë”, “Elif”, “Ftesa” etj.
+
+Gati 40 vjet është marrë me redaktimin gjuhësor të mbi 300 titujve veprash shkencore, letrare dhe didaktike, kryesisht me përmbajtje islame. Ka redaktuar edhe të gjitha tekstet shkollore të përgatitura për nxënësit e Medresesë “Isa Beu”. Ka qenë anëtar i redaksisë së“Hënës së Re” dhe lektor i saj. Anëtar i Kryesisë dhe nënkryetar i O.H.”El-Hilal”, në kuadër të BIM. Është ndër bashkëpunëtorët e parë të Enciklopedisë Islame, me seli në Stamboll.
+
+Ka bashkëpunuar me Institutin Albanologjik të Prishtinës, me AIITC-në (Instituti Shqiptar i Mendimit dhe Qytetërimit Islam-Tiranë), dhe së fundi është autor i disa zërave në Fjalorin Enciklopedik të Kosovës (botim i Akademisë së Shkencave dhe Arteve), 2018.
+
+DISA SHËNIME BIBLIOGRAFIKE:
+Ka përkthyer nga boshnjakishtja “Hadithe të zgjedhura të Pejgamberit a.s.” me 1977 hadithe dhe i ribotuar 5 herë. (Botimi i pestë doli në maj të v. 2021) Më 2015 ka botuar Antologjinë “Shllime Shpirti”, me poezi të përshpirtshme dhe atdhedashurie (780 faqe) me të cilën autori ka fituar çmimin e AIITC në edicionin e 9-të të vitit 2016. Islami- rruga e lumturisë, botuar në Shkup, në Stamboll dhe Prishtinë (mbi 300.000 kopje). Bajro Perva, “Zulija”(poemë), përkthyer më 2016, botoi Focus-Print Shkup.
+
+Më 2018, botoi studimin –temën e magjistraturës “Gurbeti në krijimtarinë gojore të shqiptarëve të Maqedonisë”, ndihmuar nga Ministria e Kulturës, botuar nga Focus-Print Shkup. Në v. 2022 doli nga shtypi monografia kushtuar Medresesë dhe medresantëve me titull Yjet e Medresesë “ISA BEU”, në 37-vjetorin e hapjes së kësaj Medreseje. PREJ NAIMIT GJER TE NAIMI është vepra me shkrime origjinale dhe kontribute tjera që botohet me rastin e 72-vjetorit të lindjes së autorit. Është një përmbledhje kumtesash të lexuara në konferenca shkencore që nga viti 1990 gjer në vitin 2022. Ka edhe një sërë shkrimesh që botohen për herë të parë. Botues: Forumi Rinor Islam. Shtypi: Furkan Print -Shkup (2022).
+
+Gati për shtyp ka librin e fundit e radhës ku ka përfshirë shkrimet e botuara nëpër gazeta e revista të ndryshme gjatë 33 vjetëve të fundit, me titull “33 VJET DITË E NET”, në 14 tabakë shkrimi me të cilën vepër ka aplikuar në konkursin e Ministrisë së Kulturës për vitin 2025.`}
+      image="/images/biografi1.jpg"
     />
   );
 };
