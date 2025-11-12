ae7e9c591452be421bcb3b6fb38dcd75980b7cf5
redbil19 <redbil19@htl-shkoder.com>
2025-11-12 08:56:05 +0100

    Biografi 7

--- src/pages/biografi/SeadRamadani.tsx
+++ src/pages/biografi/SeadRamadani.tsx
@@ -11,28 +11,49 @@ Data e lindjes: 15.01.1966
 Vendlindja: Fshati Sllupçan, rrethina e Kumanovës
 
 Arsimi:
-Shkollën fillore e kreu në Sllupçan, në shkollën “Jeta e re” (tani: Faik Konica)
-Shkollën e mesme e përfundoi në Kumanovë
+- Shkollën fillore e kreu në Sllupçan, në shkollën “Jeta e re” (tani: Faik Konica)
+- Shkollën e mesme e përfundoi në Kumanovë
 
 Puna dhe Angazhimet Profesionale:
-I punësuar si profesionist në fabrikë që nga viti 1987
-I angazhuar në aktivitete kombëtare dhe fetare që nga viti 1986
-Themelues i BRD-së (Bashkimi Rinor Demokratik) në vitin 1990
-Themelues dhe aktivist i Lidhjes së Rinisë Islame (1991)
-Anëtar i Këshillit për Pajtim në Kumanovë dhe rrethinë (1991)
+- I punësuar si profesionist në fabrikë që nga viti 1987
+- I angazhuar në aktivitete kombëtare dhe fetare që nga viti 1986
+- Themelues i BRD-së (Bashkimi Rinor Demokratik) në vitin 1990
+- Themelues dhe aktivist i Lidhjes së Rinisë Islame (1991)
+- Anëtar i Këshillit për Pajtim në Kumanovë dhe rrethinë (1991)
 
 Kontributi në Çështjet Kombëtare:
-Kontribues në përgatitjet e fushatave gjatë viteve 1990 deri në fillimin e konfliktit ndërmjet UÇK-së dhe forcave serbo-sllave
-Efektiv i stafit organizativ gjatë konfliktit të vitit 2001
-Komandant i mobilizimit në kuadër të komandës së Brigadës 113 “Ismet Jashari – Kumanova”
-Shef i Komisionit për rregullimin dhe rivarrimin e varrezave të dëshmorëve
-Mbështetës i Ushtrisë Çlirimtare të Kosovës me ndihmë nga miqtë turq
-Themelues dhe organizator i Bashkimit Demokratik për Integrim pas luftës
+- Kontribues në përgatitjet e fushatave gjatë viteve 1990 deri në fillimin e konfliktit ndërmjet UÇK-së dhe forcave serbo-sllave
+- Efektiv i stafit organizativ gjatë konfliktit të vitit 2001
+- Komandant i mobilizimit në kuadër të komandës së Brigadës 113 “Ismet Jashari – Kumanova”
+- Shef i Komisionit për rregullimin dhe rivarrimin e varrezave të dëshmorëve
+- Mbështetës i Ushtrisë Çlirimtare të Kosovës me ndihmë nga miqtë turq
+- Themelues dhe organizator i Bashkimit Demokratik për Integrim pas luftës
 
 Veprimtaria Shoqërore dhe Humanitare:
-Themelues i Shoqatës për Edukim “Vllaznia” në Kumanovë (2001), ku ishte kryetar për dy mandate
-Pjesëmarrës dhe organizator në aksione humanitare ndërkombëtare në Somali, Keni, Nigeri, Palestinë (Gazza), Bejrut, Izmir, Elazig, Kahraman Marash, Siri
-Aktualisht: Aktivist në disa organizata joqeveritare.`}
+- Themelues i Shoqatës për Edukim “Vllaznia” në Kumanovë (2001), ku ishte kryetar për dy mandate
+- Pjesëmarrës dhe organizator në aksione humanitare ndërkombëtare në:
+  • Somali – hapja e puseve për ujë të pijshëm dhe shpërndarje ndihmash ushqimore
+  • Keni – ndihma humanitare në kampin ndërkombëtar të refugjatëve në Dadab (OKB)
+  • Nigeri – ndërhyrje mjekësore për operimin e kataraktit
+  • Palestinë (Gazza) – pjesëmarrës në projektin Mavi Marmara; u arrestua nga forcat izraelite
+  • Bejrut – ndihma pas shpërthimit të madh
+  • Turqi (Izmir, Elazig, Kahraman Marash) – ndihma pas tërmeteve katastrofale
+  • Siri – ndihmë në kampet e refugjatëve në Kilis, Rejhanllë dhe zonat tejkufitare
+- Organizator i ndihmave pas tërmetit në Shqipëri
+- Aktiv në sanimin e dëmeve nga vërshimet në Shkodër dhe Skenderaj
+
+Aktivitete të tjera:
+- Iniciator dhe realizues i botimit të disa librave të përzgjedhur
+- Organizator i garave sportive për fëmijët në shkollat e rrethinës së Kumanovës
+- Realizues i dy filmave dokumentarë për funksionimin e mektebeve në Maqedoni dhe dokumentarit për Jakup ef. Asipin
+- Pjesëmarrës në dy dokumentarë të prodhuar me Al Jazeera:
+  • “Maqedonia – Lumi që na ndan”
+  • “Mbi objektet e vjetra fetare në Ballkan”
+
+Aktualisht:
+- Aktivist në disa organizata joqeveritare, duke vepruar në sektorin civil.
+- Vendndodhja: Kumanovë
+- Data: 16.12.2024`}
       image={seadImg}
     />
   );
