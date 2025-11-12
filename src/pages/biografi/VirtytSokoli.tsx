fc79d3edf07aea895c1edf7fc887220e5c9675c9
redbil19 <redbil19@htl-shkoder.com>
2025-11-12 08:49:37 +0100

    Biografi

--- src/pages/biografi/VirtytSokoli.tsx
+++ src/pages/biografi/VirtytSokoli.tsx
@@ -6,10 +6,31 @@ const VirtytSokoli = () => {
     <BiografiTemplate
       name="Virtyt Lutfi Sokoli"
       title="Avokat dhe Aktivist"
-      content={`Virtyt Lutfi Sokoli ka lindur më 24 janar 1970 në Shkodër, në një familje me traditë të spikatur arsimtare, qytetare dhe atdhetare.
-Arsimin fillor e kreu në shkollën “Ismail Qemali” dhe të mesëm në shkollën profesionale “Arben Broci”.
+      content={`Virtyt Lutfi Sokoli ka lindur më 24 janar 1970 në Shkodër, në një familje me traditë të spikatur arsimtare, qytetare dhe atdhetare. Babai i tij, një mësues i njohur i Historisë dhe Gjeografisë, ishte gjithashtu një aktivist i dalluar për demokracinë dhe e përfundoi karrierën si Drejtor i Shkollës “Branko Kadia”. Nëna e tij, Rejhan (Rroji) Sokoli, mësuese e gjuhës dhe letërsisë, ka qenë ndër themelueset dhe mësueset e para të Medresesë së Shkodrës. Nga familja ka trashëguar dashurinë për vendin, lirinë, dijen, përkushtimin qytetar dhe ndershmërinë në marrëdhëniet njerëzore.
+
+Arsimi dhe Formimi Profesional
+Arsimin fillor e kreu në shkollën “Ismail Qemali”, ku u dallua për rezultatet e larta dhe pjesëmarrjen aktive në aktivitete kulturore, sportive dhe artistike.
+Arsimin e mesëm e vazhdoi në shkollën profesionale “Arben Broci”, ku spikati për aftësitë teknike dhe talentin e tij, duke marrë pjesë në konkurse e trajnime të ndryshme.
 Studimet e larta i përfundoi në Fakultetin e Drejtësisë, Universiteti i Shkodrës “Luigj Gurakuqi”, duke u diplomuar në vitin 1996.
-Ka punuar dhe angazhuar në projekte sociale dhe juridike në Shqipëri dhe jashtë vendit, duke kontribuar në zhvillimin e komunitetit dhe çështje patriotike.`}
+Gjatë periudhës së studimeve, u angazhua si aktivist në proceset politike demokratike në qytet, angazhim që e vazhdoi deri në përfundimin e studimeve.
+
+Përvoja Profesionale dhe Shoqërore
+1997–2002: Për shkak të situatës së pasigurt në Shqipëri, emigroi dhe gjatë kësaj periudhe mori pjesë në një sërë projektesh social-artistike dhe aktivitete pune, me theks në lidership dhe organizim komunitar.
+U përfshi në projekte ndërkombëtare si “Solidariteti” dhe “Forum për Dialog”, duke trajtuar problematika historike e kulturore të Ballkanit dhe më gjerë.
+2002: U rikthye në Shqipëri dhe në vitin 2004 nisi ushtrimin e profesionit të avokatit.
+2005: Kandidoi në zgjedhjet parlamentare me “Lëvizjen për Zhvillim Kombëtar” të udhëhequr nga Leka Zogu.
+2006–2007: Shërbeu si jurist në Spitalin Rajonal Shkodër dhe pranë Myftinisë së qytetit, duke kontribuar në çështje të rëndësishme pronësore të Komunitetit Mysliman.
+I angazhuar vazhdimisht në çështje të identitetit kombëtar dhe qytetarisë shqiptare, ai është aktiv në diskutime publike për ruajtjen e traditave dhe vlerave patriotike.
+
+Karriera Institucionale dhe Akademike
+2008: Emërohet në Agjencinë Qendrore të Kthimit dhe Kompensimit të Pronave në Tiranë, ku drejtoi sektorët e Marrëdhënieve me Publikun, Juridik dhe të Apelimit. Shërbeu gjithashtu si Koordinator i Përgjithshëm i institucionit.
+2011: Përfundoi studimet pasuniversitare, duke marrë titullin Master në Administrim Publik nga Universiteti Ekonomik i Tiranës.
+2013 e në vazhdim: I rikthyer profesionit të avokatit, ushtron aktivitetin në fushën penale dhe civile, kryesisht në gjykatat e Tiranës.
+
+Aktivitete dhe Angazhime Shtesë
+Ka marrë pjesë në specializime, konferenca kombëtare dhe ndërkombëtare, ku ka referuar mbi tema historike, juridike dhe patriotike.
+Ka qenë pjesë e grupeve të punës pranë ish-Ministrisë së Integrimit, ku ka kontribuar në hartimin e pyetësorit të Bashkimit Evropian për liberalizimin e vizave.
+Vijon të jetë i angazhuar në jetën shoqërore, politike dhe qytetare, duke e konsideruar si detyrë kombëtare pjesëmarrjen në proceset e shtetformimit dhe përfaqësimin dinjitoz të çështjes shqiptare.`}
       image={virtytImg}
     />
   );
